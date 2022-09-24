4b8edca62d80e7295d485946da09921bf3c68e10
Alice Nouhouemale <alicenouhouemale@Alices-MacBook-Pro.local>
2022-09-24 06:39:22 -0700

    update

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,3 +2,32 @@
 var apiKey = "d1e2d0763204896fd894698f5c6e27ee";
 var today = moment().format('L');
 var searchHistoryList = [];
+
+// Function for current condition
+
+function currentCondition(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(cityWeatherResponse) {
+        console.log(cityWeatherResponse);
+        
+        $("#weatherContent").css("display", "block");
+        $("#cityDetail").empty();
+        
+        var iconCode = cityWeatherResponse.weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/w/${iconCode}.png";
+
+        // Displays city name, date, weather condition (temperature, humidity and wind speed with their icon) here!
+        var currentCity = $(`
+            <h2 id="currentCity">
+                ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+            </h2>
+            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+            <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
+            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+        `);
+
+        $("#cityDetail").append(currentCity);
